a3ddf761798378e612607e4c241a2af7be7db10f
Marupa-sudo <raman.aparna@gmail.com>
2024-11-27 12:54:02 +0000

    Update javamodified.groovy

--- vars/javamodified.groovy
+++ vars/javamodified.groovy
@@ -39,7 +39,7 @@ def call(Map config) {
                         container('owasp') {
                             def owaspReport = "${appName}-${TAG}.json"
                             sh """
-                            dependency-check.sh --project "${appName}" \
+                            /opt/dependency-check/bin/dependency-check.sh --project "${appName}" \
                             --scan "." --format "HTML,JSON" \
                             --out $owaspReport
                             """
